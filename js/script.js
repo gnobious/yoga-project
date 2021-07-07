591eb495902c837d532e4e87f633c566740d416b
Roman <rimer93@gmail.com>
2021-07-07 11:21:10 +0300

    Fixes, timer, JS descriptions, favicon, documentation
    - Fixed bugs (Calculator, Sending forms);
    - Change the countdown to 24 hours;
    - Added code comments;
    - Added favicon;
    - A task that is not related to the project was deleted;
    - The documentation for the project has been written.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    //  Tabs
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
-        
+    
+    //  Hiding the contents of the tabs
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -12,7 +16,9 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    hideTabContent(1);
+    hideTabContent(1);  //  Hides all tabs except the first one
+
+    //  Showing the contents of the current tab
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
@@ -21,13 +27,15 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    //  Changing tabs by clicking
+
     info.addEventListener('click', function(event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
             for(let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
-                     hideTabContent(0);
-                     showTabContent(i);
+                     hideTabContent(0);  // Hides everything                    
+                     showTabContent(i);  // Shows the required tab
                      break;
                 }
             }
@@ -35,46 +43,47 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     //Timer
-
-    let deadline = '2021-12-31T00:00:00';
+    
+    let now = new Date(),                                                            // Getting the date of the next day
+        deadline = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);   // and make it a deadline
 
     function getTimeRemaining(endtime) {
-        let t  = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
+        let t  = Date.parse(endtime) - Date.parse(new Date()),  // Сalculate the remaining time
+            seconds = Math.floor((t/1000) % 60),                // and convert it to the timer format 
             minutes = Math.floor((t/1000)/60 % 60),
             hours = Math.floor(t/(1000*60*60));
 
-        return {
-            'total' : t,
+        return {                // return formatted data 
+            'total' : t,        // in the form of an object 
             'seconds' : seconds,
             'minutes' : minutes,
             'hours' : hours
         };
     }
 
-    function setClock(id, endtime) {
+    function setClock(id, endtime) {                // set the timer to an end time and assign an identifier
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds =  timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
+            timeInterval = setInterval(updateClock, 1000);  // updating values every second 
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
             
-            function addZero(num) {
+            function addZero(num) {     // Add zero in front of numbers with one character 
                 if (num <= 9) {
                     return '0' + num; 
                 } else return num;
             }
 
-            hours.textContent = addZero(t.hours);
+            hours.textContent = addZero(t.hours);      // Outputting timer values 
             minutes.textContent = addZero(t.minutes);
             seconds.textContent = addZero(t.seconds);
 
 
 
-            if (t.total <= 0) {
+            if (t.total <= 0) {                        // Preventing negative timer values
                 clearInterval(timeInterval);
                 hours.textContent = '00';
                 minutes.textContent = '00';
@@ -85,7 +94,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', deadline);
 
-    //Modal
+    //  Modal
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
@@ -93,13 +102,13 @@ window.addEventListener('DOMContentLoaded', function() {
         tabMoreBtns = document.querySelectorAll('.description-btn'),
         index, tempBtn;
 
-        more.addEventListener('click', function() {
-            overlay.style.display = 'block';
-            this.classList.add('more-splash');
-            document.body.style.overflow = 'hidden';
+        more.addEventListener('click', function() {     // The appearance of a modal window 
+            overlay.style.display = 'block';            // Showing the window       
+            this.classList.add('more-splash');          // appearance effect 
+            document.body.style.overflow = 'hidden';    // block scrolling
         });
 
-        for (index = 0; index < tabMoreBtns.length; index++) {
+        for (index = 0; index < tabMoreBtns.length; index++) {  //same for all similar tab buttons 
             tempBtn = tabMoreBtns[index];
             tempBtn.addEventListener('click', function () {
                 overlay.style.display = 'block';
@@ -109,8 +118,8 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         }
 
-        close.addEventListener('click', function() {
-            overlay.style.display = 'none';
+        close.addEventListener('click', function() {    //closing windows
+            overlay.style.display = 'none';             //and removing the corresponding classes 
             more.classList.remove('more-splash');
             for (index = 0; index < tabMoreBtns.length; index++){
                 tabMoreBtns[index].classList.remove('more-splash');
@@ -120,7 +129,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Form
 
-    let message = {
+    let message = {             //  possible sending statuses 
         loading: 'Загрузка...',
         success: 'Спасибо! Скоро мы с вами свяжемся!',
         failure: 'Что-то пошло не так...'         
@@ -133,9 +142,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
         statusMessage.classList.add('status');
 
-    function sendForm(elem) {                               //Отправка данных форм с использованием promise и AJAX
-        elem.addEventListener('submit', function(event) {
-            event.preventDefault();
+    function sendForm(elem) {   //Sending form data using promise and AJAX
+        elem.addEventListener('submit', function(e) {
+            e.preventDefault();
             elem.appendChild(statusMessage);
             let formData = new FormData(elem);
 
@@ -150,13 +159,20 @@ window.addEventListener('DOMContentLoaded', function() {
                     request.onreadystatechange = function() {
                         if (request.readyState < 4) {
                             resolve();                                                
-                        } else if(request.readyState === 4 && request.status == 200) {
-                            resolve();
+                        } else if(request.readyState === 4) {
+                            if (request.status >= 200 && request.status < 300) {
+                                resolve();                                
+                            }                            
                         } else {
                             reject();
                         }
                     };
-                    request.send(data);
+                    let obj = {};
+                        data.forEach(function(value,key) {
+                            obj [key] = value;              //  Formatting data in JSON before sending 
+                        });
+                        let json = JSON.stringify(obj);
+                    request.send(json);
                 });
             } //End of postData
             function clearInput() {
@@ -165,21 +181,21 @@ window.addEventListener('DOMContentLoaded', function() {
                 }                
             }
 
-            postData(formData)                                                  // Получение статуса через promise
+            postData(formData)  // Getting the status via promise
                 .then(()=> statusMessage.innerHTML = message.loading)
                 .then(()=> statusMessage.innerHTML = message.success)
                 .catch(()=> statusMessage.innerHTML = message.failure)
-                .then(clearInput)
-                .then(setTimeout((()=> statusMessage.innerHTML = ''), 10000)); // Автоматическое очищение статуса через 10 сек
+                .then(clearInput)       //  Сlearing input regardless of the result
+                .then(setTimeout((()=> statusMessage.innerHTML = ''), 10000));  // Automatic status clearing after 10 seconds
         });
     }
 
     sendForm(form);
     sendForm(formBottom);
 
-    //Slider
+    //  Slider
 
-    let slideIndex = 1,
+    let slideIndex = 1,     //  Set the number for the current slide 
         slides = document.querySelectorAll('.slider-item'),
         prev = document.querySelector('.prev'),
         next = document.querySelector('.next'),
@@ -189,16 +205,16 @@ window.addEventListener('DOMContentLoaded', function() {
     showSlides(slideIndex);
     function showSlides(n) {
 
-        if (n > slides.length) {
+        if (n > slides.length) {        //  Loop the last slide
             slideIndex = 1;
         }
-        if (n < 1) {
+        if (n < 1) {                    //  with the first 
             slideIndex = slides.length;
         }
-
+        // Hide all slides and dots...
         slides.forEach((item) => item.style.display = 'none');
         dots.forEach((item) => item.classList.remove('dot-active'));
-
+        // ...and show only the first[zero]
         slides[slideIndex - 1].style.display = 'block';
         dots[slideIndex - 1].classList.add('dot-active');
     }
@@ -210,15 +226,15 @@ window.addEventListener('DOMContentLoaded', function() {
         showSlides(slideIndex = n);
     }
 
-    prev.addEventListener('click', function() {
-        plusSlides(-1);
+    prev.addEventListener('click', function() { 
+        plusSlides(-1);     // previous slide
     });
 
     next.addEventListener('click', function() {
-        plusSlides(1);
+        plusSlides(1);      // next slide 
     });
 
-    dotsWrap.addEventListener('click', function() {
+    dotsWrap.addEventListener('click', function() {     //Slide show according to the pressed dot
         for (let i = 0; i < dots.length + 1; i++) {
             if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
                 currentSlide(i);
@@ -226,7 +242,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Calc
+    // Calculator
 
     let persons = document.querySelectorAll('.counter-block-input')[0],
         restDays = document.querySelectorAll('.counter-block-input')[1],
@@ -234,13 +250,15 @@ window.addEventListener('DOMContentLoaded', function() {
         totalValue = document.getElementById('total'),
         personsSum = 0,
         daysSum = 0,
+        ratio = 1,
         total = 0;
 
         totalValue.innerHTML = 0;
-
+            //  As each variable changes, the total
+            //  is recalculated using a formula.
         persons.addEventListener('change', function() {
             personsSum = +this.value;
-            total = (daysSum * personsSum)*4000;
+            total = ((daysSum * personsSum)*4000)*ratio;
 
             if(restDays.value == '' || persons.value == '') {
                 totalValue.innerHTML = 0;
@@ -251,7 +269,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         restDays.addEventListener('change', function() {
             daysSum = +this.value;
-            total = (daysSum * personsSum)*4000;
+            total = ((daysSum * personsSum)*4000)*ratio;
 
             if(persons.value == '' || restDays.value == '') {
                 totalValue.innerHTML = 0;
@@ -264,19 +282,9 @@ window.addEventListener('DOMContentLoaded', function() {
             if (restDays.value == '' || persons.value == '') {
                 totalValue.innerHTML = 0;
             } else {
-                let a = total;
-                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+                //  The coefficient is taken from the value of the "place" option
+                ratio = this.options[this.selectedIndex].value 
+                totalValue.innerHTML = ((daysSum * personsSum)*4000)*ratio;
             }
         });
-});   
-
-            /*  //Formatting data to json
-            let obj = {};
-            formData.forEach(function(value,key) {
-                obj [key] = value;
-            });
-            let json = JSON.stringify(obj);
-
-            request.send(json); */
-
-                
+});
